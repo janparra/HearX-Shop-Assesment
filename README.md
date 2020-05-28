ac8d3d86189b95bff27e7845c8fd96cf53bec596
janparra <Diamantengoud@gmail.com>
2020-05-28 13:33:27 +0200

    initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# HearX assesment done by Jp Pretorius
+
+# Hear X assesment
+State management in anuglar applications using ngrx/store
+
+## Prerequisites for HearX assesment
+- [Angular](https://angular.io)
+- [Express](https://expressjs.com/)
+
+
+## Getting started with the assesment.
+- Clone the project and install dependencies:
+- Enter the project folder
+- Run npm install 
+- Start server by running `node server`
+- Run `ng serve` to start the Angular dev server 
+
+
+## Built With.
+
+* [Angular](https://angular.io)
+* [Express](https://expressjs.com/)
+* [ngrx/store](https://ngrx.io/guide/store)
+* [ngrx/effects](https://ngrx.io/guide/effects)
---
+++ products.js
@@ -0,0 +1,79 @@
+module.exports = [
+    {
+      "id": 1,
+      "image:": "https://picsum.photos/200",
+      "name": "Coffee Beans - 1KG Dark Roast"
+    },
+    {
+      "id": 2,
+      "image:": "https://picsum.photos/200",
+      "name": "Yorkshire tea - 100 tea bags"
+    },
+    {
+      "id": 3,
+      "image:": "https://picsum.photos/200",
+      "name": "Altered Carbon Season 2"
+    },
+    {
+      "id": 4,
+      "image:": "https://picsum.photos/200",
+      "name": "Sony WF-1000XM3 True Wireless Noise Cancelation"
+    },
+    {
+      "id": 5,
+      "image:": "https://picsum.photos/200",
+      "name": "Macbook pro 2019 model 16gb RAM"
+    },
+    {
+      "id": 6,
+      "image:": "https://picsum.photos/200",
+      "name": "Asus GL752 Gaming Laptop"
+    },
+    {
+      "id": 7,
+      "image:": "https://picsum.photos/200",
+      "name": "Guiness Draught cans 440ml 2x six packs"
+    },
+    {
+      "id": 8,
+      "image:": "https://picsum.photos/200",
+      "name": "6 Man Tent - Dark Blue"
+    }
+
+  // {
+  //   "name": "Berries",
+  //   "price": 23.54,
+  //   "image": "/assets/images/berries.jpeg",
+  //   "description": "The bestest fruit known to man. Sweet yet sour but beautiful"
+  // },
+  // {
+  //   "name": "Orange",
+  //   "price": 10.33,
+  //   "image": "/assets/images/oranges.jpeg",
+  //   "description": "Succulent and watery, you'll never run out of water"
+  // },
+  // {
+  //   "name": "Lemons",
+  //   "price": 12.13,
+  //   "image": "/assets/images/lemons.jpeg",
+  //   "description": "Sour but important for revitalization"
+  // },
+  // {
+  //   "name": "Bananas",
+  //   "price": 10.33,
+  //   "image": "/assets/images/banana.jpeg",
+  //   "description": "An every day fruit, can be served with every dish"
+  // },
+  // {
+  //   "name": "Apples",
+  //   "price": 10.33,
+  //   "image": "/assets/images/apple-item.png",
+  //   "description": "Sliced and served with your salad. Served as snacks midway through the day"
+  // },
+  // {
+  //   "name": "Sharifa",
+  //   "price": 10.33,
+  //   "image": "/assets/images/unknown.jpeg",
+  //   "description": "A great fruit, also known as custard apple"
+  // }
+]
